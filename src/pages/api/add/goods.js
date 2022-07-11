17b7b1d4982f9a377fcb1c379bb17e2b8ccab1da
faruqmaulana <faruqchodot21@gmail.com>
2022-07-11 19:19:38 +0700

    feat: add rest api

---
+++ src/pages/api/add/goods.js
@@ -0,0 +1,21 @@
+import prisma from "src/lib/prisma";
+
+export default async function addUsers(req, res) {
+  if (req.method !== "POST") return res.status(405).end();
+  const { ...data } = req.body;
+  console.log(data)
+  try {
+    await prisma.goods.create({ data });
+    res.status(200);
+    res.json({
+      message: "Data successfully added",
+      data
+    });
+  } catch (error) {
+    console.log(error)
+    res.status(400);
+    res.json({
+      message: "Failed insert data to database",
+    });
+  }
+}
