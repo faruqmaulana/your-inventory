c7f13ea25e729dd072b7004e2673ecd7881535ec
faruqmaulana <faruqchodot21@gmail.com>
2022-09-15 12:00:45 +0700

    build: change navigation

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -61,19 +61,20 @@ const navigation = () => {
       icon: AccountGroup,
       path: '/users'
     },
-    {
-      sectionTitle: 'Pages'
-    },
-    {
-      title: 'Login',
-      icon: Login,
-      path: '/pages/login'
-    },
-    {
-      title: 'Register',
-      icon: AccountPlusOutline,
-      path: '/pages/register'
-    },
+
+    // {
+    //   sectionTitle: 'Pages'
+    // },
+    // {
+    //   title: 'Login',
+    //   icon: Login,
+    //   path: '/pages/login'
+    // },
+    // {
+    //   title: 'Register',
+    //   icon: AccountPlusOutline,
+    //   path: '/pages/register'
+    // },
   ]
 }
 
